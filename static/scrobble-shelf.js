b92e39a9ec9ce755e63a5c1324d7e446a0fc6f07
David Cako <dc@cako.io>
2022-09-14 08:18:14 -0600

    fix touch double-tap behavior, implement promise for populateShelf

--- static/scrobble-shelf.js
+++ static/scrobble-shelf.js
@@ -20,45 +20,54 @@ function shelfItem(albumObj) {
     return item;
 }
 
-var IS_TOUCH = false;
-var LAST_TOUCHED = undefined;
-
-document.addEventListener("touchstart", e => {
-    IS_TOUCH = true;
-})
-
-document.getElementById("scrobble-shelf").addEventListener("click", e=> {
-    console.log(e);
-    if (IS_TOUCH && e.target.matches(".album-link")) {
-        if (LAST_TOUCHED !== e.target) {
-            LAST_TOUCHED = e.target;
-            e.preventDefault();
-        }
-    }
-})
-
 function appendItems(shelfJson) {
     var shelfObj = document.getElementById("scrobble-shelf");
     for (var i = 0; i < shelfJson.length; i++) {
         shelfObj.appendChild(shelfItem(shelfJson[i]));
     }
-} 
-
-function populateShelf() {
-    var shelfJson = [];
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status === 200) {
-                shelfJson = JSON.parse(xhr.responseText);
-                appendItems(shelfJson);
-            } else {
-                console.log("error fetching scrobble-shelf json.");
-            }
-        }
-    };
-    xhr.open("GET", pathRoot + "scrobble_shelf.json", true);
-    xhr.send();
 }
 
-populateShelf();
+function populateShelf() {
+    var promise = new Promise((resolve, reject) => {
+        var shelfJson = [];
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === XMLHttpRequest.DONE) {
+                if (xhr.status === 200) {
+                    shelfJson = JSON.parse(xhr.responseText);
+                    appendItems(shelfJson);
+
+                    resolve();
+                } else {
+                    var err = "error fetching scrobble-shelf json.";
+                    reject(err);
+                }
+            }
+        };
+        xhr.open("GET", pathRoot + "scrobble_shelf.json", true);
+        xhr.send();
+    });
+
+    return promise;
+}
+
+(async () => {
+    await populateShelf();
+
+    var IS_TOUCH = false;
+    var LAST_TOUCHED = undefined;
+
+    document.addEventListener("touchstart", e => {
+        IS_TOUCH = true;
+    })
+
+    document.querySelectorAll(".album-link").forEach(
+        elem => elem.addEventListener("click", e => {
+            console.log(e);
+            if (IS_TOUCH && LAST_TOUCHED !== e.target) {
+                LAST_TOUCHED = e.target;
+                e.preventDefault();
+            }
+        })
+    );
+})();

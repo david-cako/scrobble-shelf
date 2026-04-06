0aa7fa4db96cbe58c17485cbe36d30a6290f1cea
David Cako <dc@cako.io>
2026-04-06 10:33:44 -0600

    add command line option for http-root

--- scrobble_shelf/scrobble_shelf.py
+++ scrobble_shelf/scrobble_shelf.py
@@ -9,13 +9,15 @@ API_SECRET = os.environ['LAST_FM_API_SECRET']
 parser = argparse.ArgumentParser()
 parser.add_argument('input_json_path')
 parser.add_argument('output_dir', metavar='[output directory]')
+parser.add_argument('--http-root', nargs='?', default='/var/www')
 
 class ScrobbleShelf():
-    def __init__(self, input_albums, output_dir):
+    def __init__(self, input_albums, output_dir, http_root):
         self.metadata = [] 
         self.network = pylast.LastFMNetwork(api_key=API_KEY, api_secret=API_SECRET)
         self.albums = input_albums
         self.output_dir = output_dir
+        self.http_root = http_root
         self.cover_art_path = os.path.join(output_dir, 'cover_art')
         self.cover_art_local_path = os.path.join(output_dir, 'cover_art_local')
 
@@ -46,9 +48,9 @@ class ScrobbleShelf():
                 cover_art_local = "/" + os.path.relpath(
                     os.path.join(
                         self.cover_art_local_path, 
-                        "".join(c for c in album.title if c.isalnum()).rstrip()) + ".jpg", "/var/www")
+                        "".join(c for c in album.title if c.isalnum()).rstrip()) + ".jpg", self.http_root)
 
-                if os.path.exists("/var/www" + cover_art_local):
+                if os.path.exists(self.http_root + cover_art_local):
                     cover_art_output = cover_art_local
                     print("add cover art for {} at {}".format(album.title, cover_art_output))
                 else:
@@ -75,7 +77,7 @@ class ScrobbleShelf():
                             
                     cover_art_output = "/" + os.path.relpath(
                         os.path.join(self.cover_art_path, filename),
-                        "/var/www")
+                        self.http_root)
 
                 self.metadata.append({
                     "index": i,
@@ -90,8 +92,8 @@ class ScrobbleShelf():
                 cover_art_local = "/" + os.path.relpath(
                     os.path.join(
                         self.cover_art_path, 
-                        "".join(c for c in input_album[0] if c.isalnum()).rstrip()) + ".jpg", "/var/www")
-                if os.path.exists("/var/www" + cover_art_local):
+                        "".join(c for c in input_album[0] if c.isalnum()).rstrip()) + ".jpg", self.http_root)
+                if os.path.exists(self.http_root + cover_art_local):
                     cover_art_output = cover_art_local
                     print("add cover art for {} at {}".format(input_album[0], cover_art_output))
 
@@ -115,7 +117,7 @@ def entry_point():
         return
     with open(args['input_json_path'], 'r') as f:
         input_albums = json.load(f)
-    scrobble_shelf = ScrobbleShelf(input_albums, args['output_dir'])
+    scrobble_shelf = ScrobbleShelf(input_albums, args['output_dir'], args['http_root'])
     scrobble_shelf.create_json()
 
 
